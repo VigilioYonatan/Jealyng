4dd0d6379ec1806482cc6f983c77dc91677b78bb
VIGILIO <yonatanvigiliolavado09gmail.com>
2022-03-17 22:45:33 -0500

    configuracion MVC PHP

---
+++ public/index.php
@@ -0,0 +1,12 @@
+<?php
+require_once __DIR__.'/../includes/app.php';
+
+use MVC\Router;
+use Controller\UsuarioController;
+$router = new Router();
+
+
+$router->get('/', [UsuarioController::class, 'inicio']);
+
+
+$router->comprobarRutas();
