b554b6049804f90aa198344edcdad26176ac5630
jesus <jesus@gmail.com>
2022-03-22 00:52:19 -0500

    cambios en header y footer

--- views/web/includes/footer.php
+++ views/web/includes/footer.php
@@ -1,3 +1,4 @@
 <h3>Esto es el footer</h3>
 <h3>lorena</h3>
+<span>jeshu por aqui otra vez</span>
 <h4>Soy un virus xddd</h4>
--- views/web/includes/header.php
+++ views/web/includes/header.php
@@ -1,3 +1,8 @@
 <h1>Esto es el header</h1>
 <span>hola</span>
 <span>jeshu </span>
+<span>jeshu2 </span>
+<span>jeshu3 </span>
+<span>jeshu4</span>
+<span>jeshu5</span>
+<span>jeshu6 </span>
