54ecad5c25c9d3cd6b86015dc2bb9d11bba92497
lorena <lorenacarosoria@gmail.com>
2022-03-23 01:07:00 -0500

    agregue carpeta sass

--- views/layout.php
+++ views/layout.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jealyng</title>
-    <!-- aca va el css  -->
+    <link rel="stylesheet" href="../build/css/index.css">
 </head>
 <body>
     <?= $contenido; ?>
