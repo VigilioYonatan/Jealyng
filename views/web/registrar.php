88b99fb508fb790de7b80182731f85bce1b0125d
VIGILIO <yonatanvigiliolavado09gmail.com>
2022-03-25 21:10:45 -0500

    formulario login register add css responsibve

---
+++ views/web/registrar.php
@@ -0,0 +1,59 @@
+<?php require_once __DIR__ . '/includes/header.php'; ?>
+<section class="container">
+    <div class="formulario">
+        <form action="" class="form">
+            <h4 class="form__title">REGISTRAR</h4>
+            <div class="form-prop">
+                <label class="form-prop__lbl" for="nombre">Nombre:</label>
+                <div class="form-prop-inp">
+                    <svg width="30px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+
+                        <path
+                            d="M224 256c70.7 0 128-57.31 128-128s-57.3-128-128-128C153.3 0 96 57.31 96 128S153.3 256 224 256zM274.7 304H173.3C77.61 304 0 381.6 0 477.3c0 19.14 15.52 34.67 34.66 34.67h378.7C432.5 512 448 496.5 448 477.3C448 381.6 370.4 304 274.7 304z" />
+                    </svg>
+                    <input class="form-prop-inp__input" type="text" placeholder="Nombre de usuario..." id="nombre">
+                </div>
+            </div>
+            <div class="form-prop">
+                <label class="form-prop__lbl" for="correo">Correo Electrónico:</label>
+                <div class="form-prop-inp">
+                    <svg width="30px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                        <path
+                            d="M207.8 20.73c-93.45 18.32-168.7 93.66-187 187.1c-27.64 140.9 68.65 266.2 199.1 285.1c19.01 2.888 36.17-12.26 36.17-31.49l.0001-.6631c0-15.74-11.44-28.88-26.84-31.24c-84.35-12.98-149.2-86.13-149.2-174.2c0-102.9 88.61-185.5 193.4-175.4c91.54 8.869 158.6 91.25 158.6 183.2l0 16.16c0 22.09-17.94 40.05-40 40.05s-40.01-17.96-40.01-40.05v-120.1c0-8.847-7.161-16.02-16.01-16.02l-31.98 .0036c-7.299 0-13.2 4.992-15.12 11.68c-24.85-12.15-54.24-16.38-86.06-5.106c-38.75 13.73-68.12 48.91-73.72 89.64c-9.483 69.01 43.81 128 110.9 128c26.44 0 50.43-9.544 69.59-24.88c24 31.3 65.23 48.69 109.4 37.49C465.2 369.3 496 324.1 495.1 277.2V256.3C495.1 107.1 361.2-9.332 207.8 20.73zM239.1 304.3c-26.47 0-48-21.56-48-48.05s21.53-48.05 48-48.05s48 21.56 48 48.05S266.5 304.3 239.1 304.3z" />
+                    </svg>
+                    <input class="form-prop-inp__input" type="text" placeholder="Correo Electrónico..." id="correo">
+                </div>
+            </div>
+            <div class="form-prop">
+                <label class="form-prop__lbl" for="password">Contraseña:</label>
+                <div class="form-prop-inp">
+                    <svg width="30px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                        <path
+                            d="M80 192V144C80 64.47 144.5 0 224 0C303.5 0 368 64.47 368 144V192H384C419.3 192 448 220.7 448 256V448C448 483.3 419.3 512 384 512H64C28.65 512 0 483.3 0 448V256C0 220.7 28.65 192 64 192H80zM144 192H304V144C304 99.82 268.2 64 224 64C179.8 64 144 99.82 144 144V192z" />
+                    </svg>
+                    <input class="form-prop-inp__input" type="password" placeholder="Contraseña" id="password">
+                    <svg id="eye-open" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+                        <path
+                            d="M279.6 160.4C282.4 160.1 285.2 160 288 160C341 160 384 202.1 384 256C384 309 341 352 288 352C234.1 352 192 309 192 256C192 253.2 192.1 250.4 192.4 247.6C201.7 252.1 212.5 256 224 256C259.3 256 288 227.3 288 192C288 180.5 284.1 169.7 279.6 160.4zM480.6 112.6C527.4 156 558.7 207.1 573.5 243.7C576.8 251.6 576.8 260.4 573.5 268.3C558.7 304 527.4 355.1 480.6 399.4C433.5 443.2 368.8 480 288 480C207.2 480 142.5 443.2 95.42 399.4C48.62 355.1 17.34 304 2.461 268.3C-.8205 260.4-.8205 251.6 2.461 243.7C17.34 207.1 48.62 156 95.42 112.6C142.5 68.84 207.2 32 288 32C368.8 32 433.5 68.84 480.6 112.6V112.6zM288 112C208.5 112 144 176.5 144 256C144 335.5 208.5 400 288 400C367.5 400 432 335.5 432 256C432 176.5 367.5 112 288 112z" />
+                    </svg>
+                </div>
+            </div>
+            <div class="form-prop">
+                <label class="form-prop__lbl" for="password">Repita Contraseña:</label>
+                <div class="form-prop-inp">
+                    <svg width="30px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                        <path
+                            d="M80 192V144C80 64.47 144.5 0 224 0C303.5 0 368 64.47 368 144V192H384C419.3 192 448 220.7 448 256V448C448 483.3 419.3 512 384 512H64C28.65 512 0 483.3 0 448V256C0 220.7 28.65 192 64 192H80zM144 192H304V144C304 99.82 268.2 64 224 64C179.8 64 144 99.82 144 144V192z" />
+                    </svg>
+                    <input class="form-prop-inp__input" type="password" placeholder="Contraseña" id="password">
+                </div>
+            </div>
+            <button class="form__btn">Registrar</button>
+        </form>
+        <picture class="formulario-img">
+            <img src="./build/img/Woman Interior.webp" alt="">
+        </picture>
+    </div>
+</section>
+
+<?php require_once __DIR__ . '/includes/footer.php'; ?>
