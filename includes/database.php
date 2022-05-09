1fc2094af568bfd8a5706f1b8f17e4618d36d555
VIGILIO <yonatanvigiliolavado09gmail.com>
2022-05-09 12:38:38 -0500

    deployed heroku

--- includes/database.php
+++ includes/database.php
@@ -1,10 +1,7 @@
 <?php
-const host = 'localhost';
-const user = 'root';
-const password = '';
-const db = 'jealyng';
 
-$cnx = mysqli_connect(host, user, password, db);
+
+$cnx = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_BD']);
 
 if (!$cnx) {
     echo "<script>alert('Error en la base de datos')</script>";
